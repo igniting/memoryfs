3e80d0003e54f08061c05c86bc600de075050cbb
Anshu Avinash <anshu.avinash35@gmail.com>
2015-06-15 02:22:21 +0530

    Add definition of createFile

--- src/Data/MemoryFS.hs
+++ src/Data/MemoryFS.hs
@@ -1,5 +1,6 @@
 module Data.MemoryFS where
 
+import           Data.List.Split          (splitOn)
 import qualified Data.Map.Strict          as M
 import           Data.MemoryFS.FileSystem
 
@@ -12,9 +13,28 @@ data Node a = Leaf NodeId a
 data MemoryFS a = MemoryFS { diskView     :: Node a
                            , osView       :: Node a
                            , processView  :: Node a
-                           }
+                           } deriving (Show, Eq)
+
+getNodeIds :: Path -> [NodeId]
+getNodeIds = splitOn "/"
 
 instance FileSystem MemoryFS where
-  createFile = undefined
+  createFile fs name contents = do
+    node <- createFile' (processView fs) (getNodeIds name) contents
+    return $ MemoryFS node node node
+    where
+      createFile' :: Node a -> [NodeId] -> a -> Maybe (Node a)
+      createFile' _ [] _      = Nothing
+      createFile' Leaf{} _ _  = Nothing
+      createFile' _ [_] _     = Nothing
+      createFile' (Node nid m) (x:y:xs) c
+        | nid /= x  = Nothing
+        | null xs   = case M.lookup y m of
+                           Nothing  -> Just $ Node nid (M.insert y (Leaf y c) m)
+                           Just _   -> Nothing
+        | otherwise = do n <- M.lookup y m
+                         n' <- createFile' n (y:xs) c
+                         return $ Node nid (M.insert y n' m)
+
   renameFile = undefined
   createDirectory = undefined
